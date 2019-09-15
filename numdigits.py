b72925da1ba5291b1b8b3b90bb129deb074983aa
monsieurverde <domverdemgmt@gmail.com>
2019-09-14 21:11:52 -0400

    a few minor additions

--- numdigits.py
+++ numdigits.py
@@ -1,11 +1,15 @@
 def find_num_digits(n):
-    n_str = str(n)
+    number = str(n)
     return len(n_str)
 
 def main():
-    print(find_num_digits(50))
-    print(find_num_digits(20000))
-    print(find_num_digits(1))
+    test1 = find_num_digits(50)
+    test2 = find_num_digits(20000)
+    test3 = find_num_digits(1)
+
+    print(test1)
+    print(test2)
+    print(test3)
 
 if __name__ == "__main__":
-    main()
+    main()x
